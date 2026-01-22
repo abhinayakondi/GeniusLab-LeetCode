acacf7810d0d8ff6bc66f701ee2843e2e39b86a1
abhinayakondi <abhisonukondi123@gmail.com>
2026-01-22 16:43:14 -0500

    LeetCode first problem

---
+++ src/LeetCode 75/LC1768mergeStringsAlternately.java
@@ -0,0 +1,30 @@
+class LC1768mergeStringsAlternately {
+    public String mergeAlternately(String word1, String word2) {
+        String merged="";
+        int len1=word1.length();
+        int len2=word2.length();
+        int len= (len1>=len2)? len1 : len2;
+        int i,j;
+        char c1='\u0000';
+        char c2='\u0000';
+        for(i=0;i<len;i++){
+            if (i<len1){
+                c1=word1.charAt(i);
+                merged=merged+c1;
+            }
+            if (i<len2){
+                c2=word2.charAt(i);
+                merged=merged+c2;
+            }
+        }
+        return merged;
+    }
+
+    public static void main(String[] args){
+        LC1768mergeStringsAlternately obj=new LC1768mergeStringsAlternately();
+        String word1="abc";
+        String word2="pqr";
+        String merged=obj.mergeAlternately(word1,word2);
+        System.out.println(merged);
+    }
+}
